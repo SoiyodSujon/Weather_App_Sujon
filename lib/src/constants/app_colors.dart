6343fbde2306ce5a704b8ef123604c1fbfc2a7fa
SoiyodSujon <sujan15-14701@diu.edu.bd>
2024-05-08 23:05:10 +0600

    sujon

---
+++ lib/src/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const rainBlueLight = Color.fromARGB(255, 68, 198, 85);
+  static const rainBlueDark = Color.fromARGB(255, 120, 54, 153);
+  static const rainGradient = [rainBlueLight, rainBlueDark];
+  static const accentColor = Color(0xFFe96e50);
+}
